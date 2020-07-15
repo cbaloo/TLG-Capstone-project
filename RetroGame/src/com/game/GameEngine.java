8d8ad1200c2c1d94664ad2589e900b00b0d912e4
KushalG <xushalx@gmail.com>
2020-07-15 16:18:49 -0700

    Feature: Create Player using the name entered in the console

--- RetroGame/src/com/game/GameEngine.java
+++ RetroGame/src/com/game/GameEngine.java
@@ -1,5 +1,31 @@
 package com.game;
 
+import java.io.Console;
+
 public class GameEngine {
+    public Player player;
+    public  Console console=System.console();
+
+    //Method to start the game
+    public void start(){
+        Player player=getPlayer();
+        System.out.println(player);
+
+    }
+
+
+    //Get player name from the console and then return a Player
+    public Player getPlayer(){
+        String playerName;
+        while(true){
+            playerName=console.readLine("What do you want your name to be: ");
+            if(!playerName.isEmpty()){
+                return new Player(playerName);
+            }
+        }
+    }
+
+
+
 
 }
