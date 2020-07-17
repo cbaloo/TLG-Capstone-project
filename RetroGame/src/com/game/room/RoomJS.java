68266edfb29789022daa571327ceeb7bed3ffddd
Kervin <KervinRodriguez@me.com>
2020-07-17 15:11:05 -0700

    more quiz questions added

--- RetroGame/src/com/game/room/RoomJS.java
+++ RetroGame/src/com/game/room/RoomJS.java
@@ -15,7 +15,7 @@ public class RoomJS extends Room {
             put("JavaScript can be used with CSS and '----' to create interactive web pages.", "html");
             put("JavaScript was first known as LiveScript or ECMAScript?", "livescript");
             put("In JavaScript, semi-colons are required", "false");
-            put("", "");
+            put("JSON stands for JavaScript Object '--------'", "notation");
             put("", "");
         }
     };
--- RetroGame/src/com/game/room/RoomPython.java
+++ RetroGame/src/com/game/room/RoomPython.java
@@ -2,10 +2,24 @@ package com.game.room;
 
 import com.game.person.Instructor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RoomPython extends Room {
     //INSTANCE VARIABLES
     private String roomMessage = "Welcome to the Python Classroom!";
 
+    private Map<String,String> roomQuiz=new HashMap<>(){
+        {
+            put("a dictionary in Python contains a '---' value pair.", "key");
+            put("API stands for Application Programming '---------'","interface");
+            put("How do you comment out one single line of Python code","#");
+            put("What is the correct file extension for Python files? '.pyt' or '.py'",".py");
+            put("What method is used to return a string in upper case letters? upper() or uppercase()?","upper()");
+            put("","");
+            put("","");
+        }
+    };
     //CONSTRUCTOR
     public RoomPython(){
         super();
