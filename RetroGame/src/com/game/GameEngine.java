8f06fe77b1f395f39354eb57bcf8136685279540
KushalG <xushalx@gmail.com>
2020-07-16 19:36:43 -0700

    cleaned the merge red ink

--- RetroGame/src/com/game/GameEngine.java
+++ RetroGame/src/com/game/GameEngine.java
@@ -7,7 +7,6 @@ import java.io.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.logging.LoggingPermission;
 
 public class GameEngine {
     public Player player;
@@ -25,31 +24,12 @@ public class GameEngine {
 
     //Method to start the game
     public void start(){
-<<<<<<< HEAD
-        Player player=getPlayer();
-        //System.out.println(player);
-        //TODO Display Welcome message(with name) and Game Intro, displayIntro() maybe, actions displayed eg, [ENTER]
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-        //TODO Display the map of floor of TLG displayMap() Chandana
-        displayWelcomeMsg(player.getName());
->>>>>>> 28a7e281508491379edfbc8c77f25b962d873bc8
-=======
-=======
+
         Player player= getPlayer();
->>>>>>> c04b7d5b747c09aeae674d7a3610d850785223b7
         displayWelcomeMsg(player.getName());
         //TODO Display the map of floor of TLG displayMap() Chandana
->>>>>>> 9f3e4de090f3c6841c08ccd930b41b2b86382374
 
         //TODO Game loop starts once the player opts to ENTER
-<<<<<<< HEAD
-        while(true){
-            //TODO once in the lobby message,actions and status displayed, only java and breakout is open for entry, all others are closed with a  smart-ass comment
-            //TODO once inside java, Jay greets you the jay way, Maybe icebreaker in the beginning and then on to quiz, maybe make the presence of classmates and Jeanette known somehow
-            //TODO pass/stuck-in-java after quiz, if pass python opens and the same pattern follows, go to lobby and then to python
-=======
         if("ENTER".equals(console.readLine("Type action:").toUpperCase())){
             for(Room curRoom:roomList){
                 curRoom.action.put(curRoom.getName().value(),true);
@@ -68,7 +48,6 @@ public class GameEngine {
                 //TODO once inside java, Jay greets you the jay way, Maybe icebreaker in the beginning and then on to quiz, maybe make the presence of classmates and Jeanette known somehow
                 //TODO pass/stuck-in-java after quiz, if pass python opens and the same pattern follows, go to lobby and then to python
 
->>>>>>> c04b7d5b747c09aeae674d7a3610d850785223b7
         }
     }
 
@@ -88,10 +67,7 @@ public class GameEngine {
         }
     }
 
-<<<<<<< HEAD
-    public void enterRoom(){
-        return;
-=======
+
     //Display Welcome message and give a rundown of how to play the game
     public void displayWelcomeMsg(String name){
         System.out.println("\n\nHEY "+name.toUpperCase()+"! \n\nWELCOME TO THE TLG LEARNING FACILITY IN BELLEVUE, WA!\nCONGRATULATIONS!!! YOU ARE A BRAND SPANKING NEW SDE APPRENTICE. BE READY FOR AN EXCITING RIDE TO AMAZON. \n\nWARNING!!! BEFORE ENTERING BUCKLE UP WITH LOADS OF ENTHUSIASM. TO GET TO OJT YOU HAVE TO GO THROUGH THE GATEKEEPERS: TLG INSTRUCTORS AND STAFF.\n\nGOOD LUCK AND GODSPEED.\n\nACTIONS:[ENTER,]");
@@ -105,10 +81,8 @@ public class GameEngine {
     //Display available actions to the player
     public void displayActions(){
 
->>>>>>> c04b7d5b747c09aeae674d7a3610d850785223b7
     }
-<<<<<<< HEAD
-=======
+
     //Display player status
     public void displayStatus(){
 
@@ -122,7 +96,5 @@ public class GameEngine {
     public void displayMap(){
 
     }
->>>>>>> 28a7e281508491379edfbc8c77f25b962d873bc8
-
 
 }
