9996519126aecd77890a04e07d79725f4e34d514
Kervin <KervinRodriguez@me.com>
2020-07-17 13:00:39 -0700

    some quiz questions

--- RetroGame/src/com/game/room/RoomJS.java
+++ RetroGame/src/com/game/room/RoomJS.java
@@ -2,9 +2,23 @@ package com.game.room;
 
 import com.game.person.Instructor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RoomJS extends Room {
     //INSTANCE VARIABLES
     private String roomMessage = "Welcome to JavaScript! Are you confused yet??";
+
+    private Map<String,String> roomQuiz=new HashMap<>(){
+        {
+            put("Which company developed JavaScript, Netgear or Netscape?", "netscape");
+            put("","");
+            put("","");
+            put("","");
+            put("","");
+            put("","");
+        }
+    };
 
     //CONSTRUCTOR
     public RoomJS(){
