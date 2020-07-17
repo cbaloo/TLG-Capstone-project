407631c8e06db348890e4610b67b33e8854aca44
Kervin <KervinRodriguez@me.com>
2020-07-17 14:14:12 -0700

    more quiz questions added

--- RetroGame/src/com/game/room/RoomCapstone.java
+++ RetroGame/src/com/game/room/RoomCapstone.java
@@ -2,12 +2,26 @@ package com.game.room;
 
 import com.game.person.Instructor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RoomCapstone extends Room {
     //INSTANCE VARIABLE
     private String roomMessage = "Welcome to the Capstone classroom! You are almost worthy!!";
 
+    private Map<String, String> roomQuiz = new HashMap<>() {
+        {
+            put("How many principles does Agile have?", "12");
+            put("In Agile, the highest priority is to satisfy the '--------'.", "customer");
+            put("A user story is a tool used in '-----' software development", "agile");
+            put("When using Agile, sprints typically last between 1 and 4 '-----'.", "weeks");
+            put("Working software is the primary measure of '--------'?", "");
+            put("", "");
+        }
+    };
+
     //CONSTRUCTOR
-    public RoomCapstone(){
+    public RoomCapstone() {
         super();
         setName(Name.CAPSTONE);
         setInstructor(new Instructor("Rennie"));
